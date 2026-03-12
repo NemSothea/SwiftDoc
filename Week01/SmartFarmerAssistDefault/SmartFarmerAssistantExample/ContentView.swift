527aba641bebf9bbfd89d24d4fefb8575e7356ce
Sothea <nemsothea13@gmail.com>
2026-03-12 22:08:06 +0700

    update file,doc,projects in Week01

---
+++ Week01/SmartFarmerAssistDefault/SmartFarmerAssistantExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartFarmerAssistantExample
+//
+//  Created by sothea007 on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+          Text("Hello, World!")
+                .font(.largeTitle)
+        }
+    }
+}
+
+
